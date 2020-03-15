9064f9f9b045c739f081bda83bccc5ae616f47bc
Nathan Huber <Nathan_Huber@yahoo.com>
2020-03-15 02:07:15 -0500

    Header update.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="header" >
+      <h1>
+        Employee Tracker
+      </h1>
+      <p>
+        Click on a column heading and it allows you to sort in ascending or descending orders.
+        You can also search by first or last name if you choose.
+      </p>
+    </div>
+  )
+}
